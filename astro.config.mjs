44c1edb3dbc4e7a16c6baa3dee5743c5547c6f8b
Ragnar English <RagnarArdal@gmail.com>
2026-06-07 11:10:03 +0100

    Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   output: "server",
+  adapter: netlify(),
   integrations: [
     react(),
-    tailwind(),
   ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
